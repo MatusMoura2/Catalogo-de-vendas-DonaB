0d8a37574c5e919b29d114afbabcd166f74177cc
MatusMoura2 <matus.moura333x2@gmail.com>
2023-07-02 07:45:09 -0300

    iniciando backend

---
+++ src/main/java/com/matusmoura/catalogodonab/CatalogoDonaBApplication.java
@@ -0,0 +1,13 @@
+package com.matusmoura.catalogodonab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDonaBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDonaBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matusmoura/catalogodonab/CatalogoDonaBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matusmoura.catalogodonab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDonaBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
