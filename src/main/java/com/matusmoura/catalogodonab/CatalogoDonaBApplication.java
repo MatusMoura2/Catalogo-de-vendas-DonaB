c6b048646d81202869f8608a465194b98e4186b6
MatusMoura2 <matus.moura333x2@gmail.com>
2023-07-02 22:44:22 -0300

    criação da classe de controle e implementação do aplication.propitespara a integração com banco de dados

--- src/main/java/com/matusmoura/catalogodonab/CatalogoDonaBApplication.java
+++ src/main/java/com/matusmoura/catalogodonab/CatalogoDonaBApplication.java
@@ -8,6 +8,7 @@ public class CatalogoDonaBApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoDonaBApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/matusmoura/catalogodonab/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.matusmoura.catalogodonab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+	
+	@GetMapping
+	public void getAll() {
+		
+	}
+}
