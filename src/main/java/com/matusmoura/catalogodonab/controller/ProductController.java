f4aa0e3a11c43a0503f10af9eb34027e00d6f3f2
MatusMoura2 <matus.moura333x2@gmail.com>
2023-07-05 21:30:09 -0300

    criação da interface ProductReposirory para extender a JpaRepository, e atualização da classe ProductController

--- src/main/java/com/matusmoura/catalogodonab/controller/ProductController.java
+++ src/main/java/com/matusmoura/catalogodonab/controller/ProductController.java
@@ -1,17 +1,25 @@
 package com.matusmoura.catalogodonab.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matusmoura.catalogodonab.product.Product;
+import com.matusmoura.catalogodonab.product.ProductReposirory;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
 	
+	@Autowired
+	private ProductReposirory productRepository;
+	
 	@GetMapping
-	public void getAll() {
-		Product product;
+	public List<Product> getAll() {
+		List<Product> productList = productRepository.findAll();
+		return productList;
 	}
 }
---
+++ src/main/java/com/matusmoura/catalogodonab/product/ProductReposirory.java
@@ -0,0 +1,7 @@
+package com.matusmoura.catalogodonab.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReposirory extends JpaRepository<Product, Long>{
+
+}
