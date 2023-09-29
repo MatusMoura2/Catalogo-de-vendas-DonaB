ff2233957b080a52eb0d39afdf44505d08c3579d
AlehandroSV <alehandrosantos99@gmail.com>
2023-09-29 19:29:45 -0300

    feat: Configurando liberação de CORS

---
+++ src/main/java/com/matusmoura/catalogodonab/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.matusmoura.catalogodonab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Para fazer Deploy mudar URL da Origin
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
