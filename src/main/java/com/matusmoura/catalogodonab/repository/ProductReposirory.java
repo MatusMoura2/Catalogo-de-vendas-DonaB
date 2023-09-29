296bc504393bf337a4dc820ad5561c3f4e2f6ec8
AlehandroSV <alehandrosantos99@gmail.com>
2023-09-29 19:57:01 -0300

    fix: Ajustando Id de Long para UUID

--- src/main/java/com/matusmoura/catalogodonab/repository/ProductReposirory.java
+++ src/main/java/com/matusmoura/catalogodonab/repository/ProductReposirory.java
@@ -1,9 +1,11 @@
 package com.matusmoura.catalogodonab.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matusmoura.catalogodonab.model.Product;
 
-public interface ProductReposirory extends JpaRepository<Product, Long> {
+public interface ProductReposirory extends JpaRepository<Product, UUID> {
 
 }
